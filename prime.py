3f654e8c4924b0586fd85bc8d82680299f65a011
Peter Frost <mail@pfrost.me>
2022-05-10 22:29:28 +0100

    Refactor Python example, use array instead of list

--- prime.py
+++ prime.py
@@ -1,9 +1,8 @@
-import time, math                                                               # Gives us system timing functions and math functions like sqrt and floor
+#!/bin/python3
+import math                                                                     # Gives us math functions like sqrt and floor
 import multiprocessing, itertools                                               # Gives us multithreading related functions
 import sys                                                                      # Gives us command line argument functions
-
-primeList = [0]                                                                 # Global variable that is a list [] of integers
-
+import array as arr                                                             # Gives us arrays
 
 
 # Factor test by trial division using the 6k +- 1 optimisation, this
@@ -32,40 +31,39 @@ def findFactors(testNum, verbose):
                 if (verbose): print("%d divides by %d" % (testNum, divisor + 2))
                 isPrime = False
 
-    return isPrime
-
+    if isPrime == True:
+        return testNum
+    else:
+        return 0
 
 
 # Helper function for calculating all prime numbers up to maxNumber
 #
 def primeListTest(maxNumber):
-    global primeList                                                            # Specifies that we want to use the global variable rather than make a new one with the same name
+    numberList = arr.array('I', range(1, maxNumber + 1))                        # Prepares an array from 1 to maxNumber (inclusive)
+    chunkSize = min((maxNumber / multiprocessing.cpu_count()), 10000)           # From experimentation using chunks over 10000 significantly hurts performance
+    numPrimes = 0
 
-    numProcessors = multiprocessing.cpu_count()                                 # Gets the number of threads of the CPU
-    numberList = range(1, maxNumber + 1)                                        # Prepares a list from 0 to maxNumber (inclusive)
-    primeList = [0] * maxNumber                                                 # This isn't quite the same as dynamic memory allocation, but creates an array of maxNumber elements
-    numPrimes = 0                                                               # Arrays start at 0 in Python
+    with multiprocessing.Pool(multiprocessing.cpu_count()) as p:                # Spawns multiple instances of Python to compute parts of the loop in parralel
+        # starmap allows us to pass an iterable with multiple elements so we can
+        # make use of the verbose argument, if we removed that we could write
+        # primeList = p.map(findFactors, numberList, chunksize=chunkSize) which is ~10% faster
+        primeList = p.starmap(findFactors, zip(numberList, itertools.repeat(False)), chunksize=chunkSize)
 
-    with multiprocessing.Pool(numProcessors) as p:                              # Spawns multiple instances of Python to compute parts of the loop in parralel
-        for i, isPrime in zip(numberList, p.starmap(findFactors, \
-                          zip(numberList, itertools.repeat(False)))):           # zip makes a single list out of separate arguments, p.starmap calls findFactors with those arguments
-            if (isPrime == True):
-                primeList[numPrimes] = i                                        # Arrays start at 0 in Python
-                numPrimes += 1                                                  # We don't need mutex locks here because they are completely separate processes with their own memory
+    for i in primeList:                                                         # This loop essentially removes the blanks and bunches all the primes up next to eachother in primeList
+        if (i != 0):
+            primeList[numPrimes] = i
+            numPrimes += 1
 
-    return numPrimes
-
-
+    return primeList, numPrimes
 
 
 # Called by the slightly odd if statement below
 #
 def main():
     maxNumber = int(sys.argv[1])                                                # argv[0] will be the py file, the first argument is argv[1]
-    numPrimes = primeListTest(maxNumber)
+    primeList, numPrimes = primeListTest(maxNumber)
     print("Generated %d primes, Largest was: %d " % (numPrimes, primeList[numPrimes - 1]))
-
-
 
 
 # This will be the start of the program as it's the first bit that's
